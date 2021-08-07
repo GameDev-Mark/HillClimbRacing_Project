d7c62c5381a63e30070f9f8fba3d74379439c042
GameDev-Mark <gamedev.markd@gmail.com>
2021-08-07 20:50:38 +0200

    update01

--- HillClimb_TestProject_01/Assets/Scripts/CarMovements.cs
+++ HillClimb_TestProject_01/Assets/Scripts/CarMovements.cs
@@ -3,19 +3,32 @@
 public class CarMovements : MonoBehaviour
 {
     float carSpeed;
+    float carReverseSpeed;
+
+    public Rigidbody2D rb;
+
+    public GameObject _groundCheckScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        carSpeed = 5f;
+        carSpeed = 4f;
+        carReverseSpeed = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D)) // move right
+        if(_groundCheckScript.GetComponent<GroundCheck>().isGrounded == true)
         {
-            transform.position = Vector3.right * carSpeed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.D)) // move right, forwards
+            {
+                rb.AddForce(transform.right * carSpeed);
+            }
+            if (Input.GetKey(KeyCode.A)) // move left, backwards
+            {
+                rb.AddForce(transform.right * -carReverseSpeed);
+            }
         }
     }
 }
---
+++ HillClimb_TestProject_01/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    [SerializeField] public bool isGrounded;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            Debug.Log("Grounded");
+        }
+        else
+        {
+            isGrounded = false;
+            Debug.Log("NOT Grounded");
+        }
+    }
+}
