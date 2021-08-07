8731d2423f3d677125723dad9074ff09024cd559
GameDev-Mark <gamedev.markd@gmail.com>
2021-08-07 17:15:07 +0200

    Initial Upload

---
+++ HillClimb_TestProject_01/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform player;
+    Vector3 camOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camOffset = new Vector3(0f, 3f, -10f);
+    }
+
+    // unitys lateUpdate function
+    void LateUpdate()
+    {
+        transform.position = player.position + camOffset;
+        transform.LookAt(player.position);
+    }   
+}
---
+++ HillClimb_TestProject_01/Assets/Scripts/CarMovements.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CarMovements : MonoBehaviour
+{
+    float carSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D)) // move right
+        {
+            transform.position = Vector3.right * carSpeed * Time.deltaTime;
+        }
+    }
+}
